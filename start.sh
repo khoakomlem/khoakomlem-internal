b552e8bf705c121a7497debbb9973f6a54c13475
Đậu Khoa <khoakomlem@gmail.com>
2025-06-14 21:31:33 +0700

    update start script

--- start.sh
+++ start.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# Load .env file if it exists
+if [ -f .env ]; then
+  export $(cat .env | grep -v '^#' | xargs)
+fi
+
+# setup.sh
+VOLUMES_PATH=${VOLUMES_PATH:-./volumes}
+
+mkdir -p "$VOLUMES_PATH/jenkins"
+mkdir -p "$VOLUMES_PATH/sonarqube/data"
+mkdir -p "$VOLUMES_PATH/sonarqube/logs"
+mkdir -p "$VOLUMES_PATH/sonarqube/extensions"
+mkdir -p "$VOLUMES_PATH/postgres"
+
+echo "Volume directories created at $VOLUMES_PATH"
+
 docker compose -f docker-compose.yml -p dev-space up -d
